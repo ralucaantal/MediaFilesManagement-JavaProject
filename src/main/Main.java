03aa82b3faa52a796dcf4255163bf803d0101d0e
Raluca Antal-Vaida <ralucaantal0609@gmail.com>
2023-11-03 18:25:47 +0200

    New functionality: actions with files

--- src/main/Main.java
+++ src/main/Main.java
@@ -72,8 +72,7 @@ public class Main {
                             System.exit(0);
                         } else if (response.equals("back")) {
                             location = "Home";
-                        }
-                        else if (response.equals("add")) {
+                        } else if (response.equals("add")) {
                             System.out.println("Type the path of the new director: ");
 
                             response = scanner.nextLine();
@@ -92,6 +91,7 @@ public class Main {
                             response = scanner.nextLine();
                             if (Double.parseDouble(response) >= 1 && Double.parseDouble(response) <= computer.getDirectories().size()) {
                                 int resp = (int) Double.parseDouble(response) - 1;
+                                System.out.println("Actual name: " + computer.getDirectories().get(resp).getPath());
                                 System.out.println("Type new name for the directory: ");
                                 response = scanner.nextLine();
                                 computer.getDirectories().get(resp).setPath(response);
@@ -158,7 +158,7 @@ public class Main {
 
                         } else {
                             System.out.println("If you want details for a specific file, type the index of the file. Write `back` to go back. Write `exit` to close the app. ");
-                            System.out.println("Or choose an action: add | rename | delete");
+                            System.out.println("Or choose an action: add | rename | delete | move | copy");
                         }
                         response = scanner.nextLine();
                         if (response.equals("exit")) {
@@ -182,7 +182,7 @@ public class Main {
 
                             String type = scanner.nextLine();
 
-                            File newFile=new File(name, type);
+                            File newFile = new File(name, type);
 
                             computer.getDirectories().get(directorNo).getFiles().add(newFile);
 
@@ -195,18 +195,129 @@ public class Main {
                                 System.out.println("-------------------------------------");
 
                             }
-                        }
-                        else if (response.equals("delete")) {
+
+                        } else if (response.equals("move")) {
+
+
+                            System.out.println("Choose the file you want to move in another location: ");
+
+                            response = scanner.nextLine();
+                            if (Double.parseDouble(response) >= 1 && Double.parseDouble(response) <= computer.getDirectories().get(directorNo).getFiles().size()) {
+                                int fileNumber = (int) Double.parseDouble(response) - 1;
+
+                                for (int i = 0; i < computer.getDirectories().size(); i++) {
+                                    System.out.println(i + 1 + ". " + computer.getDirectories().get(i).getPath());
+                                }
+
+
+                                System.out.println("\nChoose the directory where you want to move the file: ");
+
+                                response = scanner.nextLine();
+
+                                if (Double.parseDouble(response) >= 1 && Double.parseDouble(response) <= computer.getDirectories().size()) {
+                                    int destination = (int) Double.parseDouble(response) - 1;
+
+                                    computer.getDirectories().get(destination).getFiles().add(computer.getDirectories().get(directorNo).getFiles().get(fileNumber));
+
+                                    computer.getDirectories().get(directorNo).getFiles().remove(fileNumber);
+
+
+                                    if (!computer.getDirectories().get(directorNo).getFiles().isEmpty()) {
+                                        System.out.println("-------------------------------------");
+
+                                        for (int i = 0; i < computer.getDirectories().get(directorNo).getFiles().size(); i++) {
+                                            System.out.println(i + 1 + ". " + computer.getDirectories().get(directorNo).getFiles().get(i).getName());
+                                        }
+                                        System.out.println("-------------------------------------");
+
+                                    }
+                                } else {
+                                    if (!computer.getDirectories().get(directorNo).getFiles().isEmpty()) {
+                                        for (int i = 0; i < computer.getDirectories().get(directorNo).getFiles().size(); i++) {
+                                            System.out.println(i + 1 + ". " + computer.getDirectories().get(directorNo).getFiles().get(i).getName());
+                                        }
+                                        System.out.println("-------------------------------------");
+
+                                    }
+                                    throw new MultimediaException("Wrong index. Try again!");
+
+                                }
+                            } else {
+                                if (!computer.getDirectories().get(directorNo).getFiles().isEmpty()) {
+                                    for (int i = 0; i < computer.getDirectories().get(directorNo).getFiles().size(); i++) {
+                                        System.out.println(i + 1 + ". " + computer.getDirectories().get(directorNo).getFiles().get(i).getName());
+                                    }
+                                    System.out.println("-------------------------------------");
+
+                                }
+                                throw new MultimediaException("Wrong index. Try again!");
+                            }
+
+                        } else if (response.equals("copy")) {
+
+
+                            System.out.println("Choose the file you want to copy in another location: ");
+
+                            response = scanner.nextLine();
+                            if (Double.parseDouble(response) >= 1 && Double.parseDouble(response) <= computer.getDirectories().get(directorNo).getFiles().size()) {
+
+                                int fileNumber = (int) Double.parseDouble(response) - 1;
+
+                                for (int i = 0; i < computer.getDirectories().size(); i++) {
+                                    System.out.println(i + 1 + ". " + computer.getDirectories().get(i).getPath());
+                                }
+
+                                System.out.println("\nChoose the directory where you want to copy the file: ");
+
+                                response = scanner.nextLine();
+
+                                if (Double.parseDouble(response) >= 1 && Double.parseDouble(response) <= computer.getDirectories().size()) {
+
+                                    int destination = (int) Double.parseDouble(response) - 1;
+
+                                    computer.getDirectories().get(destination).getFiles().add(computer.getDirectories().get(directorNo).getFiles().get(fileNumber));
+
+                                    if (!computer.getDirectories().get(directorNo).getFiles().isEmpty()) {
+                                        System.out.println("-------------------------------------");
+
+                                        for (int i = 0; i < computer.getDirectories().get(directorNo).getFiles().size(); i++) {
+                                            System.out.println(i + 1 + ". " + computer.getDirectories().get(directorNo).getFiles().get(i).getName());
+                                        }
+                                        System.out.println("-------------------------------------");
+
+                                    }
+                                } else {
+                                    if (!computer.getDirectories().get(directorNo).getFiles().isEmpty()) {
+                                        for (int i = 0; i < computer.getDirectories().get(directorNo).getFiles().size(); i++) {
+                                            System.out.println(i + 1 + ". " + computer.getDirectories().get(directorNo).getFiles().get(i).getName());
+                                        }
+                                        System.out.println("-------------------------------------");
+
+                                    }
+                                    throw new MultimediaException("Wrong Index! Try again.");
+                                }
+
+                            } else {
+                                if (!computer.getDirectories().get(directorNo).getFiles().isEmpty()) {
+                                    for (int i = 0; i < computer.getDirectories().get(directorNo).getFiles().size(); i++) {
+                                        System.out.println(i + 1 + ". " + computer.getDirectories().get(directorNo).getFiles().get(i).getName());
+                                    }
+                                    System.out.println("-------------------------------------");
+
+                                }
+                                throw new MultimediaException("Wrong Index! Try again.");
+                            }
+                        } else if (response.equals("delete")) {
 
 
                             System.out.println("Choose the file you want to delete: ");
 
-                            response=scanner.nextLine();
+                            response = scanner.nextLine();
 
                             if (Double.parseDouble(response) >= 1 && Double.parseDouble(response) <= computer.getDirectories().get(directorNo).getFiles().size()) {
                                 int resp = (int) Double.parseDouble(response) - 1;
-                               // System.out.println(computer.getDirectories().get(directorNo).getFiles().get(resp));
-                                List<File> newList=new ArrayList<>(computer.getDirectories().get(directorNo).getFiles());
+                                // System.out.println(computer.getDirectories().get(directorNo).getFiles().get(resp));
+                                List<File> newList = new ArrayList<>(computer.getDirectories().get(directorNo).getFiles());
                                 newList.remove(resp);
                                 computer.getDirectories().get(directorNo).setFiles(newList);
                                 //computer.getDirectories().get(directorNo).getFiles().remove(resp);
@@ -222,33 +333,31 @@ public class Main {
                                 }
                                 System.out.println("-------------------------------------");
 
-                            }
-                            else {
+                            } else {
                                 System.out.println("There are no files in this directory!");
                                 System.out.println("-------------------------------------");
 
                                 System.out.println("Choose an action: add | back | exit");
                             }
 
-                        }
-                        else if (response.equals("rename")) {
+                        } else if (response.equals("rename")) {
 
 
                             System.out.println("Choose the file you want to rename: ");
 
-                            response=scanner.nextLine();
+                            response = scanner.nextLine();
 
                             if (Double.parseDouble(response) >= 1 && Double.parseDouble(response) <= computer.getDirectories().get(directorNo).getFiles().size()) {
                                 int resp = (int) Double.parseDouble(response) - 1;
-                              //  System.out.println(computer.getDirectories().get(directorNo).getFiles().get(resp));
+                                //  System.out.println(computer.getDirectories().get(directorNo).getFiles().get(resp));
 
-                                System.out.println("Actual name: "+ computer.getDirectories().get(directorNo).getFiles().get(resp).getName());
+                                System.out.println("Actual name: " + computer.getDirectories().get(directorNo).getFiles().get(resp).getName());
 
                                 System.out.println("New name: ");
 
-                                response=scanner.nextLine();
+                                response = scanner.nextLine();
 
-                                List<File> newList=new ArrayList<>(computer.getDirectories().get(directorNo).getFiles());
+                                List<File> newList = new ArrayList<>(computer.getDirectories().get(directorNo).getFiles());
                                 newList.get(resp).setName(response);
                                 computer.getDirectories().get(directorNo).setFiles(newList);
                                 //computer.getDirectories().get(directorNo).getFiles().remove(resp);
@@ -264,16 +373,14 @@ public class Main {
                                 }
                                 System.out.println("-------------------------------------");
 
-                            }
-                            else {
+                            } else {
                                 System.out.println("There are no files in this directory!");
                                 System.out.println("-------------------------------------");
 
                                 System.out.println("Choose an action: add | back | exit");
                             }
 
-                        }
-                        else if (Double.parseDouble(response) >= 1 && Double.parseDouble(response) <= computer.getDirectories().get(directorNo).getFiles().size()) {
+                        } else if (Double.parseDouble(response) >= 1 && Double.parseDouble(response) <= computer.getDirectories().get(directorNo).getFiles().size()) {
                             System.out.println("Full details for your file: ");
                             int index = (int) (Double.parseDouble(response) - 1);
                             System.out.println(computer.getDirectories().get(directorNo).getFiles().get(index));
